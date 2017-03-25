7606c94c4a510bcea1171ff15571c2a5a4cc222a
Shreyansh1997 <shreyanshghewariya1997@gmail.com>
2017-03-25 15:46:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //changing element
 var element = document.getElementById('main-text');
 element.innerHTML="Welcome";
+
+//move img
+var img=document.getElementById('img');
+img.onclick=function()
+{
+    img.style.marginLeft='100px';
+};
