103f0d6e3018b87a384ce08f035ccaa5bb921252
Shreyansh1997 <shreyanshghewariya1997@gmail.com>
2017-03-25 16:18:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 
 function moveright(){
-    marginLeft = margintLeft + 10;
+    marginLeft = margintLeft + 10 ;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
