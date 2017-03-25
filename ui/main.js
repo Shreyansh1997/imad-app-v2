4296ffa2d579b95de16be4a8686436d1589e1e34
Shreyansh1997 <shreyanshghewariya1997@gmail.com>
2017-03-25 16:11:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,13 +4,13 @@ var element = document.getElementById('main-text');
 element.innerHTML="Welcome";
 
 //move img
-var img=document.getElementById('madi');
+var img = document.getElementById('madi');
 var marginLeft = 0;
 
 function moveright(){
     marginLeft = margintLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
-img.onclick=function(){
+img.onclick = function(){
     var interval=setInterval(moveright,100);
 };
