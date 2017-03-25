11268f4a7b302c95193bf54bc60dc2cb5a2a6ad6
Shreyansh1997 <shreyanshghewariya1997@gmail.com>
2017-03-25 17:55:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,19 @@
 //Counter Code
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 button.onclick = function() {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+    
+    if(request.readystate == XMLHttpRequestDONE){
+    
+    if(request.status == 200){
+        var counter = request.responseText;
+    }
+    }    
+    };
+    request.open('GET', 'http://shreyansh1997.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
